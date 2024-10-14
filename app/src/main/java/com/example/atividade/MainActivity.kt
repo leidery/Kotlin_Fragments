fbd0f3c06c5a3444ddb153f74d916a0e6e65dc9e
Aline Bancks <87613356+leidery@users.noreply.github.com>
2024-10-14 19:23:25 -0300

    adicionado arquivos

---
+++ app/src/main/java/com/example/atividade/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.atividade
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.atividade.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.buttonBlueFragment.setOnClickListener {
+            replaceFragment(BlueFragment())
+        }
+
+        binding.buttonRedFragment.setOnClickListener {
+            replaceFragment(RedFragment())
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
